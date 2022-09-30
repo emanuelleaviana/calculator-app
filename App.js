33c5fd23ba5f3301d3b31ae663f0769f414f9b9f
Julio Sousa <juliocspfc@gmail.com>
2022-09-30 14:49:33 -0300

    Visor primeira versão

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Visor from './Componentes/Visor';
-import Teclado from './Componentes/Teclado';
-import Calculadora from './Componentes/Calculadora';
+import Visor from './Components/Visor';
+import Teclado from './Components/Teclado';
+import Calculadora from './Components/Calculadora';
 
 export default function App() {
   return (
    
   
     <Visor></Visor>
-
-  
-
+   
   );
 }
+
+
 
 
