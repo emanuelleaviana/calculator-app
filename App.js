b790d0f5c5678c9b7d608da30f0aa39ca06363fd
Emanuelle Viana <manuavbr@gmail.com>
2022-10-17 16:25:33 -0300

    updateCalculator

--- App.js
+++ App.js
@@ -1,96 +1,121 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Button, TextInput } from 'react-native';
 import React from 'react';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { Entypo } from '@expo/vector-icons';
 import { useState } from 'react';
-// import { Button } from "react-native-elements";
 
-class App extends React.Component  {
+export default function App() {
 
-    state = {
-      miniDisplay:0,
-      display:0,
+  const buttons = ['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '─', 1, 2, 3, '+', "<", 0, '.', '=']
+  const [currentNumber, setCurrentNumber] = useState(" ");
+  const [lastNumber, setLastNumber] = useState(" ");
+  const [buttonAC, setButtonAC] = useState(" ");
+
+
+  let backspace = buttons.indexOf("<");
+  if (backspace !== -1) {
+    buttons[backspace] = <TouchableOpacity onPress={() => handleInput("<")} style={styles.button}><Text style={styles.specialCharacter}><MaterialCommunityIcons name="restore" size={25} /></Text></TouchableOpacity>
+  };
+
+  let buttonPlusLess = buttons.indexOf("+/-");
+  if (buttonPlusLess !== -1) {
+    buttons[buttonPlusLess] = <TouchableOpacity onPress={() => handleInput("+/-")} style={styles.button}>
+      <Text style={[styles.text, styles.greenText, styles.plus]}>+</Text>
+      <Text style={[styles.text, styles.greenText, styles.bar]}>/</Text>
+      <Text style={[styles.text, styles.greenText, styles.less]}>-</Text>
+    </TouchableOpacity>
+  };
+
+  if (currentNumber !== "" | currentNumber !== null) {
+    let teste = buttons.indexOf("AC");
+    if (teste !== -1){
+      buttons[teste] = <TouchableOpacity onPress={() => clearValue("AC")} style={styles.button}><Text style={[styles.text, styles.greenText]}>C</Text></TouchableOpacity>
     }
+  };
 
-    click0 = () => {
-      this.setState({
-        miniDisplay: 0
-      })
+  function calculator() {
+    const divisionNumbers = currentNumber.split(' ')
+    const firstNumber = parseFloat(divisionNumbers[0])
+    const lastNumber = parseFloat(divisionNumbers[2])
+    const operator = divisionNumbers[1]
+
+    switch (operator) {
+      case '+':
+        setCurrentNumber((firstNumber + lastNumber).toString())
+        return
+      case '─':
+        setCurrentNumber((firstNumber - lastNumber).toString())
+        return
+      case 'x':
+        setCurrentNumber((firstNumber * lastNumber).toString())
+        return
+      case '÷':
+        setCurrentNumber((firstNumber / lastNumber).toString())
+        return
+      case '%':
+        setCurrentNumber((firstNumber % lastNumber).toString())
+        return
     }
+  }
 
-    click1 = () => {
-      this.setState({
-        miniDisplay: 1
-      })
+  function handleInput(buttonPressed) {
+    if (buttonPressed === '+' | buttonPressed === '─' | buttonPressed === '÷' | buttonPressed === 'x' | buttonPressed === '%') {
+      setCurrentNumber(currentNumber + " " + buttonPressed + " ")
+      return
     }
+    switch (buttonPressed) {
+      case '<':
+        setCurrentNumber(currentNumber.toString().substring(0, (currentNumber.length - 1)))
+        return
+      case '+/-':
+        setCurrentNumber(currentNumber * -1)
+        return
+      case '=':
+        setLastNumber(currentNumber + " = ")
+        calculator()
+        return
+    }
+    setCurrentNumber(currentNumber + buttonPressed)
+  }
 
-    render(){
+  function clearValue(buttonPressed) {
+    if (buttonPressed == 'AC') {
+      setLastNumber("")
+      setCurrentNumber("")
+    }
+  }
+
   return (
     <View style={styles.background}>
       <View style={styles.display}>
-        <Text style={styles.miniDisplay}>{this.state.miniDisplay}</Text>
-        <Text style={styles.textDisplay}>12936</Text>
+        <Text style={styles.miniDisplay}>{lastNumber}</Text>
+        <Text style={styles.textDisplay}>{currentNumber.toString().substring(0, 9)}</Text>
       </View>
-      <View style={styles.keyboard}>
-        <View style={styles.grade}>
-          <TouchableOpacity style={styles.button}><Text style={[styles.text, styles.greenText]}>AC</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
-            <Text style={[styles.text, styles.greenText, styles.plus]}>+</Text>
-            <Text style={[styles.text, styles.greenText, styles.bar]}>/</Text>
-            <Text style={[styles.text, styles.greenText, styles.less]}>-</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button}><Text style={[styles.text, styles.greenText]}>%</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.button}><Text style={[styles.text, styles.redText]}>÷</Text></TouchableOpacity>
-        </View>
-
-        <View style={styles.grade}>
-          <TouchableOpacity style={styles.button}><Text style={styles.text}>7</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.button}><Text style={styles.text}>8</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.button}><Text style={styles.text}>9</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.button}><Text style={[styles.text, styles.redText]}>x</Text></TouchableOpacity>
-        </View>
-
-        <View style={styles.grade}>
-          <TouchableOpacity style={styles.button}><Text style={styles.text}>4</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.button}><Text style={styles.text}>5</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.button}><Text style={styles.text}>6</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.button}><Text style={[styles.text, styles.redText]}>━</Text></TouchableOpacity>
-        </View>
-
-        <View style={styles.grade}>
-          <TouchableOpacity  onPress={this.click1} style={styles.button}><Text style={styles.text}>1</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.button}><Text style={styles.text}>2</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.button}><Text style={styles.text}>3</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.button}><Text style={[styles.text, styles.redText]}>
-            <Entypo name="plus" size={25} />
-          </Text></TouchableOpacity>
-        </View>
-
-        <View style={styles.grade}>
-          <TouchableOpacity  onPress={() => handleInput("#")} style={styles.button}><Text style={styles.specialCharacter}>
-            <MaterialCommunityIcons name="restore" size={25} />
-          </Text></TouchableOpacity>
-          <TouchableOpacity onPress={this.click0} style={styles.button}><Text style={styles.text}>0</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.button}><Text style={styles.text}>.</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.button}><Text style={[styles.text, styles.redText]}>=</Text></TouchableOpacity>
+      <View style={styles.backgroundKeyboard}>
+        <View style={styles.keyboard}>
+          {buttons.map((button) =>
+            button == '=' | button == '+' | button == '─' | button == 'x' | button == '÷' ?
+              <TouchableOpacity onPress={() => handleInput(button)} key={button} style={styles.button}><Text style={[styles.text, styles.redText]}>{button}</Text></TouchableOpacity>
+              :
+              button == '+/-' | button == '%' ?
+                <TouchableOpacity onPress={() => handleInput(button)} key={button} style={styles.button}><Text style={[styles.text, styles.greenText]}>{button}</Text></TouchableOpacity>
+                :
+                button == 'AC' ?
+                  <TouchableOpacity onPress={() => clearValue(button)} key={button} style={styles.button}><Text style={[styles.text, styles.greenText]}>{button}</Text></TouchableOpacity>
+                  :
+                  <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button]}><Text style={styles.text}>{button}</Text></TouchableOpacity>
+          )}
         </View>
       </View>
     </View>
   );
-  }
 }
+
 const styles = StyleSheet.create({
   background: {
     flex: 1,
     backgroundColor: '#22252d'
   },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  keyboard: {
+  backgroundKeyboard: {
     flex: 0.65,
     alignItems: 'center',
     alignContent: 'flex-end',
@@ -99,10 +124,11 @@ const styles = StyleSheet.create({
     borderTopStartRadius: 40,
     borderTopEndRadius: 40,
   },
-  grade: {
+  keyboard: {
     flexDirection: 'row',
     flexWrap: 'wrap',
     marginTop: 20,
+    justifyContent: 'center',
   },
   button: {
     width: 80,
@@ -111,7 +137,7 @@ const styles = StyleSheet.create({
     margin: 5,
     borderRadius: 25,
     justifyContent: 'center',
-    position:'relative',
+    position: 'relative',
   },
   text: {
     textAlign: 'center',
@@ -142,30 +168,26 @@ const styles = StyleSheet.create({
     fontSize: 70,
     textAlign: 'right',
     fontWeight: 'bold',
-    
   },
   miniDisplay: {
     color: 'white',
     fontSize: 30,
     textAlign: 'right',
-    
   },
-  bar:{
+  bar: {
     transform: [{ rotate: '18deg' }],
-    fontWeight:'normal',
+    fontWeight: 'normal',
   },
   plus: {
-    fontSize:18,
-    position:'absolute',
-    left:25,
-    top:20,
+    fontSize: 20,
+    position: 'absolute',
+    marginLeft: 25,
+    top: 18,
   },
   less: {
-    fontSize:25,
-    position:'absolute',
-    left:42,
-    top:28,
+    fontSize: 25,
+    position: 'absolute',
+    left: 42,
+    top: 28,
   },
 });
-
-export default App
